18eb313b2ccb44a55be694b5a80451b528a2c192
Ahmo934 <ahmedirfan934@gmail.com>
2026-08-09 02:05:47 +0500

    Update setup_directml.py

--- src/setup_directml.py
+++ src/setup_directml.py
@@ -29,7 +29,7 @@ if os.path.isfile(REQUIREMENTS):
     subprocess.check_call([venv_python, "-m", "pip", "install", "-r", REQUIREMENTS])
 else:
     packages = [
-        "customtkinter", "opencv-python", "pyserial", "mss", "ultralytics",
+        "customtkinter", "opencv-python", "pyserial", "mss", "ultralytics==8.3.187",
         "onnx", "onnxruntime-directml", "cyndilib", "dxcam"
     ]
     print(f"[*] {REQUIREMENTS} not found. Installing default packages: {packages}")
